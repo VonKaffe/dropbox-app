b06736972230ab810fcc576bf16045c770674e87
Simon Appeltofft <simonappeltofft@hotmail.com>
2018-06-11 13:07:55 +0200

    changed component, added key

--- dropbox-app/src/app/app.component.ts
+++ dropbox-app/src/app/app.component.ts
@@ -1,4 +1,10 @@
+
 import { Component } from '@angular/core';
+const key = "ZmrQAnZTaxAAAAAAAAAAHB6ylBPAKPlvmESb0wlz_fQcDSs0uFlM8JvzYp8m1xH7"
+
+require('isomorphic-fetch'); // or another library of choice.
+var Dropbox = require('dropbox').Dropbox;
+var dbx = new Dropbox({ accessToken: key });
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First Angular App!';
+  title = 'app';
+  entries = [];
+  constructor () {
+    dbx.filesListFolder({path: ''})
+      .then(function(response) {
+        console.log(response);
+        this.entries = response.entries;
+
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 }
